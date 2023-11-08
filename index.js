a12309dc3328bb823d19a0f0f5f842fd9d1503d7
Henrique França <109392835+RickyFranca@users.noreply.github.com>
2023-11-08 15:27:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const app = express();
+const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const agoraAppId = 'SEU_APP_ID';
+const agoraAppCertificate = 'SEU_APP_CERTIFICATE';
+
+app.get('/gerar-token', (req, res) => {
+  const { channelName, uid } = req.query;
+
+  if (!channelName || !uid) {
+    return res.status(400).json({ error: 'Parâmetros inválidos' });
+  }
+
+  const expirationTimeInSeconds = 3600; // Tempo de validade do token (1 hora)
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+
+  const token = RtcTokenBuilder.buildTokenWithUid(
+    agoraAppId,
+    agoraAppCertificate,
+    channelName,
+    uid,
+    RtcRole.PUBLISHER,
+    privilegeExpiredTs
+  );
+
+  res.json({ token });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
